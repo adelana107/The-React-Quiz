dba085770607ab8d5f0ec77ca64e2c81f9ee7496
Adelana107 <adelana787898@gmail.com>
2025-10-16 15:49:36 +0100

    Loading Questions from a Fake API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
     <div className="app">
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question?</p>
+        {state.status === "loading" && <p>Loading questions...</p>}
+        {state.status === "ready" && (
+          <>
+            <p>1/{state.questions.length}</p>
+            <p>{state.questions[0]?.question || "No questions"}</p>
+          </>
+        )}
       </Main>
     </div>
   );
